776f176f7e8562e1617e739bd1939736eaeae78f
Mason Fox <masonfox22@gmail.com>
2022-03-27 20:17:50 -0400

    initial commit

---
+++ src/components/footer.js
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+const Footer = ({ navigation }) => (
+  <footer className="bg-gray-700 mt-7">
+    <div className="max-w-7xl mx-auto py-10 px-4 overflow-hidden sm:px-6 lg:px-8">
+      <nav
+        className="-mx-5 -my-2 flex flex-wrap justify-center"
+        aria-label="Footer"
+      >
+        {navigation.map((item) => (
+          <div key={item.name} className="px-5 py-2">
+            <Link href={item.href}>
+              <a className="text-base text-white hover:text-indigo-300">
+                {item.name}
+              </a>
+            </Link>
+          </div>
+        ))}
+      </nav>
+      <p className="mt-8 text-center text-base text-gray-300">
+        &copy; {new Date().getFullYear()} Mason Fox. All rights reserved.
+      </p>
+    </div>
+  </footer>
+);
+
+export default Footer;
---
+++ src/components/layout.js
@@ -0,0 +1,20 @@
+import Navbar from './navbar'
+import Footer from './footer'
+
+const navigation = [
+  { name: "Home", href: "/", current: true },
+  { name: "Get Started", href: "/onboard", current: false },
+  { name: "Commands", href: "/commands", current: false },
+  { name: "FAQ", href: "/faq", current: false },
+  { name: "About", href: "/about", current: false },
+];
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Navbar navigation={navigation} />
+      <main>{children}</main>
+      <Footer navigation={navigation} />
+    </>
+  )
+}
