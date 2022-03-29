b4edbf0f29e930775a05b7abd4235ac0736218f1
Mason Fox <masonfox22@gmail.com>
2022-03-29 09:00:41 -0400

    add a CTA

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import Link from "next/link";
+
 import HomeHero from "../components/homehero";
 import GetStarted from "../components/getstarted";
 import Link from "next/link";
@@ -23,6 +25,32 @@ export default function Home() {
 
         {/* Commands */}
         <Commands />
+
+        {/* CTA */}
+        <div className="bg-indigo-100">
+          <div className="max-w-7xl mx-auto text-center py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+            <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+              <span>Let&apos;s get started!</span>
+              <span className="ml-3 text-indigo-600">It&apos;s <span className="underline">free</span>!</span>
+            </h2>
+            <div className="mt-8 flex justify-center">
+              <div className="inline-flex rounded-md shadow">
+                <Link href="/#getstarted">
+                  <a className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">
+                    Get started
+                  </a>
+                </Link>
+              </div>
+              <div className="ml-3 inline-flex">
+                <Link href="/#features">
+                  <a className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-white hover:bg-indigo-200">
+                    Learn more
+                  </a>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
 
         {/* FAQ */}
         <FAQ />
