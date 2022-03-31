bc7ae70589059ca6ed3f47084e133b8c6fe94270
Mason Fox <masonfox22@gmail.com>
2022-03-30 22:57:49 -0400

    additional faq's

--- src/components/faq.js
+++ src/components/faq.js
@@ -9,6 +9,11 @@ const faqs = [
       "<p>It runs on a 2-hour cadence. You can see the next time it will run by using the <span class='inline-code'>!osrs when</span> command. Note, the bot's cadence can be altered from time-to-time depending on redeploys.</p>"
   },
   {
+    question: "I progressed, but the bot didn't catch it! What gives?",
+    answer:
+      "<p>The bot is driven by Jagex's <a href='https://secure.runescape.com/m=hiscore_oldschool/overall'>hiscore</a> data source. If you're unfamiliar, the hiscore table only updates <b>once you log out</b> from an active session; it doesn't update during your game session. Therefore, even if you progress your account but don't logout before the bot runs, the hiscores will not reflect your updates and the bot can't either.</p><br/><p>If you do believe there is an error, please open an issue in the <a href='https://github.com/masonfox/osrs-discord-bot/issues'>Github repo</a>.</p>"
+  },
+  {
     question: "How do I rename a player I am tracking?",
     answer: "<p>This can be done by removing the player with the <span class='inline-code'>!osrs remove {old name}</span> command and then adding them back with the <span class='inline-code'>!osrs add {new name}</span> command by their nnew name.</p><br/><p>A note about this, doing this does remove that player's associated history. This can effect (the future) recap command's ability to provide results before the name change.</p><br/><p>While there is an open <a href='https://github.com/masonfox/osrs-discord-bot/issues/4'>issue</a> for this feature, it is unlikely to be developed due to the complexity and cascading effects against the data set. The players list is shared amongst all bot users, so granting overwrite-like functionality has dangerous potential. Many other systems similar to this simply do not allow it or have an approval process associated with it. </p>",
   },
@@ -16,6 +21,11 @@ const faqs = [
     question: "How do I change the channel I receive progress updates in?",
     answer:
       "<p>This can be done using the <span class='inline-code'>!osrs subscribe</span> command. Simply use it in the text channel you want to receive updates in and it'll overwrite the previous channel configuration.</p>"
+  },
+  {
+    question: "How does the bot work? What do you do with the data?",
+    answer:
+      "<p>The idea is really simple: it retains a 'last state' representation of your skills, bosses, and clue scroll data from Jagex's <a href='https://secure.runescape.com/m=hiscore_oldschool/overall'>hiscore</a> data, and when the bot runs, it simply compares the two to identify what progress you've made within that period of time.</p><br><p>The only OSRS data that is retained is your hiscore data (skill levels, boss kill counts, and clue scroll). From Discord, only the necessary identifiers are names for logging information are retained.</p>"
   },
   {
     question: "I have a bug or a feature request: how do I submit it?",
